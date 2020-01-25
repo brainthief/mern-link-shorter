d4e75e41849b97695f48a986ab29e0c1e7c23a1a
brainthief <vytautas1983@gmail.com>
2020-01-25 17:30:41 +0200

    konfigas developer env

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('App running')
+const express = require('express')
+const confirm = require('config')
+
+const app = express()
+
+const PORT = confirm.get('port') || 5000
+
+app.listen(PORT, () => console.log(`App has been started... ${PORT}`))
