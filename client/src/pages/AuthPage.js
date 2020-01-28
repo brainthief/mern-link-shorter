704462f949c4fb8f47f43101738de5066c1606a3
brainthief <vytautas1983@gmail.com>
2020-01-28 21:29:10 +0200

    fix dizainas

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,43 @@ import React from "react";
 
 export const AuthPage = () => {
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Link Shorter</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Authorization</span>
+            <div>
+              <div className="input-field">
+                <input
+                  placeholder="Insert email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field">
+                <input
+                  placeholder="Insert password"
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="yellow-input"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>
+              Login
+            </button>
+            <button className="btn grey lighten-1 black-text">Register</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
