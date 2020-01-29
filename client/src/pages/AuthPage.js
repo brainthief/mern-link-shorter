75cb1ad406b640a4046a6e183d1bb7900264dc99
brainthief <vytautas1983@gmail.com>
2020-01-29 21:39:12 +0200

    registracija

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import { useHttp } from "../hooks/http.hook";
 
 export const AuthPage = () => {
+  const { loading, error, request } = useHttp();
   const [form, setForm] = useState({ email: "", password: "" });
 
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const registerHandler = async () => {
+    try {
+      const data = await request("/api/auth/register", "POST", { ...form });
+      console.log("Data", data);
+    } catch (e) {}
   };
 
   return (
@@ -40,10 +49,20 @@ export const AuthPage = () => {
             </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>
+            <button
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
+              disabled={loading}
+            >
               Login
             </button>
-            <button className="btn grey lighten-1 black-text">Register</button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Register
+            </button>
           </div>
         </div>
       </div>
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,93 +1,94 @@
-const { Router } = require('express')
-const bcrypt = require('bcryptjs')
-const config = require('config')
-const jwt = require('jsonwebtoken')
-const { check, validationResult } = require('express-validator')
-const User = require('../modules/User')
-const router = Router()
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const User = require("../modules/User");
+const router = Router();
 
 // /api/auth/register - /api/auth -> prefix iš app.js
 router.post(
- '/register',
- [
-  check('email', 'Wrong password').isEmail(),
-  check('password', 'Min length 6').isLength({ min: 6 })
- ],
- async (req, res) => {
-  try {
-   const errors = validationResult(req)
+  "/register",
+  [
+    check("email", "Wrong password").isEmail(),
+    check("password", "Min length 6").isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    try {
+      console.log("Body:", req.body);
+      const errors = validationResult(req);
 
-   if (!errors.isEmpty()) {
-    return res.status(400).json({
-     errors: errors.array(),
-     message: 'Registration data if wrong'
-    })
-   }
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Registration data is wrong"
+        });
+      }
 
-   const { email, password } = req.body
+      const { email, password } = req.body;
 
-   const candidate = await User.findOne({ email })
+      const candidate = await User.findOne({ email });
 
-   if (candidate) {
-    return res.status(400).json({ message: 'Email is already used' })
-   }
+      if (candidate) {
+        return res.status(400).json({ message: "Email is already used" });
+      }
 
-   const hashedPassword = await bcrypt.hash(password, 1)
-   const user = User({ email, password: hashedPassword })
+      const hashedPassword = await bcrypt.hash(password, 1);
+      const user = User({ email, password: hashedPassword });
 
-   await user.save()
+      await user.save();
 
-   res.status(201).json({ message: 'User created successfully' })
-
-  } catch (e) {
-   //error 500
-   res.status(500).json({ message: 'Something was wrong, try again' })
+      res.status(201).json({ message: "User created successfully" });
+    } catch (e) {
+      //error 500
+      res.status(500).json({ message: "Something was wrong, try again" });
+    }
   }
- })
+);
 
 // /api/auth/login
 router.post(
- '/login',
- [
-  check('email', 'Please insert valid email address').normalizeEmail().isEmail(),
-  check('password', 'Please insert password').exists()
- ],
- async (req, res) => {
-  try {
-   const errors = validationResult(req)
+  "/login",
+  [
+    check("email", "Please insert valid email address")
+      .normalizeEmail()
+      .isEmail(),
+    check("password", "Please insert password").exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-   if (!errors.isEmpty()) {
-    return res.status(400).json({
-     errors: errors.array(),
-     message: 'Registration data if wrong'
-    })
-   }
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Registration data if wrong"
+        });
+      }
 
-   const { email, password } = req.body
+      const { email, password } = req.body;
 
-   const user = await User.findOne({ email })
+      const user = await User.findOne({ email });
 
-   if (!user) {
-    return res.status(400).json({ message: 'Wrong email or password' })
-   }
+      if (!user) {
+        return res.status(400).json({ message: "Wrong email or password" });
+      }
 
-   const isMatch = await bcrypt.compare(password, user.password)
+      const isMatch = await bcrypt.compare(password, user.password);
 
-   if (!isMatch) {
-    return res.status(400).json({ message: 'Wrong password' })
-   }
+      if (!isMatch) {
+        return res.status(400).json({ message: "Wrong password" });
+      }
 
-   const token = jwt.sign(
-    { userId: user.id },
-    config.get('jwtSecretKey'),
-    { expiresIn: '1h' }
-   )
+      const token = jwt.sign({ userId: user.id }, config.get("jwtSecretKey"), {
+        expiresIn: "1h"
+      });
 
-   res.json({ token, userId: user.id })
-
-  } catch (e) {
-   res.status(500).json({ message: 'Something was wrong, try again' })
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: "Something was wrong, try again" });
+    }
   }
- })
+);
 
-module.exports = router
+module.exports = router;
