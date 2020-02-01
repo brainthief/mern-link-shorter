08aa2c3e8570d3e499418d0b6734a6e6567348df
brainthief <vytautas1983@gmail.com>
2020-02-01 20:44:49 +0200

    links list page

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -47,6 +47,7 @@ router.get("/", auth, async (req, res) => {
 router.get("/:id", auth, async (req, res) => {
   try {
     const link = await Link.findById(req.params.id);
+    res.json(link);
   } catch (e) {
     res.status(500).json({ message: "Something was wrong" });
   }
