5301aa052f470f9ddabd630236b1bd7417dc76a6
brainthief <vytautas1983@gmail.com>
2020-02-01 16:24:52 +0200

    generavimas kodo ir create

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useHttp } from "../hooks/http.hook";
 
 export const CreatePage = () => {
- return (
-  <div>
-   <h1>Create Page</h1>
-  </div>
- )
-}
+  const { request } = useHttp();
+  const [link, setLink] = useState("");
+
+  useEffect(() => {
+    window.M.updateTextFields();
+  }, []);
+
+  const pressHandler = async event => {
+    if (event.key === "Enter") {
+      try {
+        const data = await request("/api/link/generate", "POST", {
+          from: link
+        });
+      } catch (e) {}
+    }
+  };
+
+  return (
+    <div className="row">
+      <div className="col s8 offset-s2" style={{ paddingTop: "2rem" }}>
+        <div className="input-field">
+          <input
+            placeholder="Insert URL"
+            id="link"
+            type="text"
+            name="text"
+            onChange={e => setLink(e.target.value)}
+            value={link}
+            onKeyPress={pressHandler}
+          />
+          <label htmlFor="text">Link</label>
+        </div>
+      </div>
+    </div>
+  );
+};
